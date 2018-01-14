023a857c7c0bd5751351625fa8c2adf6d270ce69
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-14 13:18:31 +0100

    msm8998: add 5s wifi driver probe delay
    Needs https://github.com/NastyAndroidDevs/android_frameworks_opt_net_wifi/commit/8a3dac1e9ec4b2f2869cbf39373e81c7effed8d8
    
    Change-Id: Iccac9aaa67a7b543e2f79af50f13fd566f79a7a7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -293,6 +293,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_PROBE_DELAY := 5
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
