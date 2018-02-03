60cb1c2658bb2727a26112debc2560f395edfb7c
liquid0624 <rjbirnesser@gmail.com>
2018-02-03 15:01:57 -0500

    msm8998: remove oneplus three doze
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- common.mk
+++ common.mk
@@ -181,10 +181,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusThreeDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
