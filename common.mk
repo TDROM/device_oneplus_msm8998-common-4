385f7d62af021552168d376a9720778d547561a9
dianlujitao <dianlujitao@lineageos.org>
2018-02-12 16:01:34 +0100

    msm8998: Fork camera provider HAL
    Change-Id: Iacdfa6e1a98cefd480bb73416f96532340a3e554

--- common.mk
+++ common.mk
@@ -138,6 +138,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service.oneplus5 \
     camera.device@3.2-impl \
     GoogleCamera
 
